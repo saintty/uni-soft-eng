aa4e06b8399c8544759eaf44a987359f8cfd0cf7
Anton Gavrilov <anton.gavrilov.1673@mail.ru>
2024-02-22 20:18:06 +0300

    sex 3

--- functions/Fibonacci/Fibonacci.test.js
+++ functions/Fibonacci/Fibonacci.test.js
@@ -1,10 +1,13 @@
-import { fib } from "../isPrime/index.js";
+import { fib } from "./index";
 
-test("test isPrime: true", () => {
-    expect(fib(2)).ToEqual();
-    expect(fib(3)).toBeTruthy();
-    expect(fib(3)).toBeTruthy();
-    expect(fib(5)).toBeTruthy();
-    expect(fib(6)).toBeTruthy();
-    expect(fib(7)).toBeTruthy();
+test("test Fib: answer correct", () => {
+    expect(fib(0)).toEqual(0);
+    expect(fib(1)).toEqual(1);
+    expect(fib(2)).toEqual(1);
+    expect(fib(3)).toEqual(2);
+    expect(fib(4)).toEqual(3);
+    expect(fib(5)).toEqual(5);
+    expect(fib(6)).toEqual(8);
+    expect(fib(-1)).toBeFalsy();
+    expect(fib("12")).toBeFalsy();
 });
