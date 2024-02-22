4fd8240757167908496363625708ce13966930a6
Artem Uzinger <aauzinger@yandex.ru>
2024-02-22 18:07:35 +0300

    Add isPrime func & test, update jest config

--- functions/isPrime/index.js
+++ functions/isPrime/index.js
@@ -1,1 +1,11 @@
-export const isPrime = () => true;
+export const isPrime = (x) => {
+  if (typeof x !== "number" || x <= 1) return false;
+
+  const sqrt = Math.sqrt(x);
+
+  for (let i = 2; i <= sqrt; i++) {
+    if (x % i === 0) return false;
+  }
+
+  return true;
+};
--- functions/isPrime/isPrime.test.js
+++ functions/isPrime/isPrime.test.js
@@ -1,5 +1,22 @@
-const isPrime = require("./index");
+import { isPrime } from "./index";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(isPrime(1)).toBe(true);
+test("test isPrime: true", () => {
+  expect(isPrime(2)).toBeTruthy();
+  expect(isPrime(3)).toBeTruthy();
+  expect(isPrime(13)).toBeTruthy();
+  expect(isPrime(11)).toBeTruthy();
+  expect(isPrime(43)).toBeTruthy();
+  expect(isPrime(1009)).toBeTruthy();
+});
+
+test("test isPrime: false", () => {
+  expect(isPrime(0)).toBeFalsy();
+  expect(isPrime(-19)).toBeFalsy();
+  expect(isPrime(-1)).toBeFalsy();
+  expect(isPrime(12)).toBeFalsy();
+  expect(isPrime(102)).toBeFalsy();
+  expect(isPrime(121)).toBeFalsy();
+  expect(isPrime("asd")).toBeFalsy();
+  expect(isPrime(null)).toBeFalsy();
+  expect(isPrime("123123")).toBeFalsy();
 });
