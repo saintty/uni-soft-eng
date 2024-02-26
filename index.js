55643cd3d3d9ffe487c91ea2ccf08b471980a0bb
Artem Uzinger <aauzinger@yandex.ru>
2024-02-26 11:13:22 +0300

    Remove unused

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 
 import showMenu from "./utils/menu.js";
-import { clearScreenDown } from "node:readline";
 
 const rl = readline.createInterface({ input, output });
 await showMenu(rl);
